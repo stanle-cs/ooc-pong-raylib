0e8d28578108b296a9f689f17ddaf97cf52d98ea
Stan Le <stanle.work@gmail.com>
2022-01-03 15:20:14 -0500

    Update main.c with a rudimentary layout of the Pong game.
    Two new Paddle objects, paddleLeft and paddleRight created. The p
    Entity2D object is used as the position for the ball, relocated to the
    middle of the screen. Screen size changed to width = 800, height = 400.

--- sources/main.c
+++ sources/main.c
@@ -1,22 +1,26 @@
+#include <string.h>
 #include <raylib.h>
 
 #include "Entity2D.h"
+#include "Paddle.h"
 
 int main(void)
 {
 	// Initialization
 	//--------------------------------------------------------------------------------------
 	initEntity2D();
+	initPaddle();
 	void * p = new(Entity2D, 1, 2);
+	void * paddleLeft = new(Paddle, 10, 160, 20, 80);
+	void * paddleRight = new(Paddle, 770, 160, 20, 80);
 	puto(p, stdout);
 	printf("The position of p is %d %d\n", get_x(p), get_y(p));
 	move(p, 2, 3);
 	printf("The position of p is %d %d\n", get_x(p), get_y(p));
-	delete(p);
 	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenHeight = 400;
 
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+	InitWindow(screenWidth, screenHeight, "Pong game with raylib and Object Oriented C");
 
 	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 	//--------------------------------------------------------------------------------------
@@ -35,7 +39,12 @@ int main(void)
 
 		ClearBackground(RAYWHITE);
 
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		//DrawLine(400, 0, 400, 400, DARKGRAY);
+		DrawText("PONG", 400 - (MeasureText("PONG", 20) / 2), 10, 20, LIGHTGRAY);
+		move(p, 400 - get_x(p), 200 - get_y(p));
+		DrawCircle(get_x(p), get_y(p), 10.0, DARKGREEN);
+		draw(paddleLeft);
+		draw(paddleRight);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
@@ -44,6 +53,9 @@ int main(void)
 	// De-Initialization
 	//--------------------------------------------------------------------------------------
 	CloseWindow();        // Close window and OpenGL context
+	delete(p);
+	delete(paddleLeft);
+	delete(paddleRight);
 	//--------------------------------------------------------------------------------------
 
 	return 0;
